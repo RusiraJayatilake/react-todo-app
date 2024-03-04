7f613dbf33b90b9b374e9378558d2a2be35edacd
rusira <rusirajayatilake96@gmail.com>
2024-03-04 20:52:50 +1300

    delete button

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import './App.css';
 import AddTodos from './components/addTodo/addTodo';
-import Sample from './components/sample';
 
 function App() {
   return (
     <div>
       <AddTodos />
-      {/* <Sample /> */}
-        {/* <header className="App-header">
-          
-        </header> */}
-      
     </div>
-    
   );
 }
 
